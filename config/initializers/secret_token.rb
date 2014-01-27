bf15c140fca8e2ba2264abb08769c8b4084066a9
DPL-Epsilon <dpl-epsilon@DPL-Epsilons-iMac.local>
2014-01-27 13:46:14 -0700

    Start new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectM::Application.config.secret_token = '616d2602942b7057bd1782ff6e703356035e88cea0677865f45944781da56b1acbdafdd1fa30ba7193d0474a07f3e650cfb69ff225c98f59bc43870c705a058c'
