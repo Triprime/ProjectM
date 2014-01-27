bb70100d3571600106978c3c192d3b9ecb1a73e8
DPL-Epsilon <dpl-epsilon@DPL-Epsilons-iMac.local>
2014-01-27 14:09:48 -0700

    Add scaffold for Project

---
+++ db/migrate/20140127210932_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :summary
+      t.text :scope
+      t.text :resources
+      t.string :team_member
+      t.string :team_member_title
+      t.string :team_member_contact
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/functional/projects_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class ProjectsControllerTest < ActionController::TestCase
+  setup do
+    @project = projects(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:projects)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create project" do
+    assert_difference('Project.count') do
+      post :create, project: { resources: @project.resources, scope: @project.scope, summary: @project.summary, team_member: @project.team_member, team_member_contact: @project.team_member_contact, team_member_title: @project.team_member_title, title: @project.title }
+    end
+
+    assert_redirected_to project_path(assigns(:project))
+  end
+
+  test "should show project" do
+    get :show, id: @project
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @project
+    assert_response :success
+  end
+
+  test "should update project" do
+    put :update, id: @project, project: { resources: @project.resources, scope: @project.scope, summary: @project.summary, team_member: @project.team_member, team_member_contact: @project.team_member_contact, team_member_title: @project.team_member_title, title: @project.title }
+    assert_redirected_to project_path(assigns(:project))
+  end
+
+  test "should destroy project" do
+    assert_difference('Project.count', -1) do
+      delete :destroy, id: @project
+    end
+
+    assert_redirected_to projects_path
+  end
+end
